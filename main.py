e649bd382ac60fae144a488fe501b1c59bbafeee
yaoyingjie <nnyc@me.com>
2023-04-21 02:13:49 +0800

    更新

--- main.py
+++ main.py
@@ -1,58 +1,85 @@
 import json
-import numpy as np
+import pandas as pd
+import requests
+import random
 
 
-# 从本地文件加载JSON数据
-def load_json_data(file_path):
-    with open(file_path, "r", encoding="utf-8") as file:
-        return json.load(file)
+def get_continuous_blocks(lst):
+    blocks = []
+    current_block = [lst[0]]
+
+    for i in range(1, len(lst)):
+        if lst[i] == lst[i - 1]:
+            current_block.append(lst[i])
+        else:
+            blocks.append(current_block)
+            current_block = [lst[i]]
+
+    blocks.append(current_block)
+
+    return blocks
 
 
-# 增加科室数量
-def expand_departments(student, departments):
-    expanded_departments = []
-    for department_id in student["department_ids"]:
-        department = next((dept for dept in departments if dept["id"] == department_id), None)
-        if department:
-            duration = int(department["rotation_duration"])
-            expanded_departments.extend([department_id] * duration)
-    return expanded_departments
+def shuffle_blocks(student_expanded_department_ids):
+    continuous_blocks = get_continuous_blocks(student_expanded_department_ids)
+    random.shuffle(continuous_blocks)
+
+    shuffled_expanded_department_ids = []
+    for block in continuous_blocks:
+        shuffled_expanded_department_ids.extend(block)
+
+    return shuffled_expanded_department_ids
 
 
-def calculate_monthly_capacity(departments, total_students):
-    total_durations = {dept["id"]: int(dept["rotation_duration"]) for dept in departments}
-    total_duration = sum(total_durations.values())
-    monthly_capacity = {dept_id: int(duration * total_students / total_duration) for dept_id, duration in
-                        total_durations.items()}
-    return monthly_capacity
+# Read students.json from URL
+students_url = "https://raw.githubusercontent.com/ajietr/cycle/main/students.json"
+students_response = requests.get(students_url)
+students_data = students_response.json()
+students = students_data["students"]
 
+# Read departments.json from URL
+departments_url = "https://raw.githubusercontent.com/ajietr/cycle/main/departments.json"
+departments_response = requests.get(departments_url)
+departments = departments_response.json()
 
-def main():
-    students_file = "students.json"
-    departments_file = "departments.json"
+# 根据科室的rotation_duration增加学生department_ids里对应科室的数量
+for student in students:
+    expanded_department_ids = []
+    for dept_id in student["department_ids"]:
+        department = next(dept for dept in departments if dept["id"] == dept_id)
+        expanded_department_ids.extend([dept_id] * int(department["rotation_duration"]))
+    student["expanded_department_ids"] = expanded_department_ids
 
-    raw_students_data = load_json_data(students_file)
-    students_data = raw_students_data["students"]  # 获取students列表
-    departments_data = load_json_data(departments_file)
+# 随机重排expanded_department_ids
+for student in students:
+    student["expanded_department_ids"] = shuffle_blocks(student["expanded_department_ids"])
 
-    expanded_students_departments = []
+# 计算总月份数
+total_months = len(students[0]["expanded_department_ids"])
 
-    for student in students_data:  # 直接遍历students列表
-        expanded_departments = expand_departments(student, departments_data)
-        expanded_students_departments.append(expanded_departments)
+# 初始化一个空的字典，用于在每个时间单位内存储每个科室的学生
+schedule = {dept["id"]: [""] * total_months for dept in departments}
 
-    total_students = len(students_data)
-    monthly_capacity = calculate_monthly_capacity(departments_data, total_students)
+# 为每个科室的每个时间单位分配学生
+for student in students:
+    for month, dept_id in enumerate(student["expanded_department_ids"]):
+        if schedule[dept_id][month]:
+            schedule[dept_id][month] += ", "
+        schedule[dept_id][month] += student["name"]
 
-    print("Monthly capacity for each department:")
-    for dept_id, capacity in monthly_capacity.items():
-        print(f"Department {dept_id}: {capacity} students")
+# 将字典转换为DataFrame
+dept_names = {dept["id"]: dept["name"] for dept in departments}
+df = pd.DataFrame(schedule)
+df.rename(columns=dept_names, inplace=True)
+df.index.name = "Month"
+df.reset_index(inplace=True)
+df["Month"] = df["Month"] + 1
 
-    rotation_matrix = np.array(expanded_students_departments)
+# 转置DataFrame
+df = df.set_index('Month').transpose().reset_index()
+df.index.name = 'Department'
+df.reset_index(inplace=True)
+df.rename(columns={"index": "Department"}, inplace=True)
 
-    print("\nRotation matrix:")
-    print(rotation_matrix)
-
-
-if __name__ == "__main__":
-    main()
+# 保存为Excel文件
+df.to_excel("rotation_schedule.xlsx", index=False, engine="openpyxl")
